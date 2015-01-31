a50169a569f59f9f08d7fcdc442a627b66a62913
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2015-01-31 12:37:06 +0000

    Initial commit

--- src/ls.java
+++ src/ls.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 /**
  * Created by Pierre on 31/01/2015.
@@ -12,6 +15,13 @@ public class ls {
     public void launch() {
         String filename = ".";
         File file = new File(filename);
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader(file));
+        } catch(FileNotFoundException ex) {
+            System.out.println("File does not exist");
+        }
         System.out.println(file.list());
+        in.close;
     }
 }
