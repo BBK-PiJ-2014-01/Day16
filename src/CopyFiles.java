7b5a7e7d42b66dcaee004120a3a126f669799cf5
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2015-01-31 23:49:57 +0000

    Initial commit

---
+++ src/CopyFiles.java
@@ -0,0 +1,13 @@
+/**
+ * Created by PierreM on 31/01/2015.
+ */
+public class CopyFiles {
+    public static void main(String[] args) {
+        CopyFiles programme = new CopyFiles();
+        programme.launch(args);
+    }
+
+    public void launch(String[] args) {
+
+    }
+}
