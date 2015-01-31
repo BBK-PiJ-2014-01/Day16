3b02ccdb85969213b616c58a1b34f0cbb12ea0e7
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2015-01-31 23:18:29 +0000

    Code Part1 complete

--- src/Cat.java
+++ src/Cat.java
@@ -1,5 +1,43 @@
+import java.io.*;
+
 /**
  * Created by Pierre on 31/01/2015.
  */
 public class Cat {
+    public static void main(String[] args) {
+        Cat programme = new Cat();
+        programme.launch(args);
+    }
+
+    public void launch(String[] args) {
+        String fileName = "";
+        BufferedReader in = null;
+        try {
+            if (args.length == 0)
+                throw new IndexOutOfBoundsException();
+            fileName += args[0];
+            File file = new File(fileName);
+            in = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch(FileNotFoundException ex) {
+            System.out.println("File "+args[0]+" not found");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            closeReader(in);
+        }
+    }
+
+    private void closeReader(Reader reader) {
+        try {
+            if (reader != null) {
+                reader.close();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
