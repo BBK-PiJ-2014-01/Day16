036b387adb481a1fbbf5fa788164c4b67d9f8914
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2015-02-02 21:53:39 +0000

    Code complete

--- src/CopyFiles.java
+++ src/CopyFiles.java
@@ -10,35 +10,42 @@ public class CopyFiles {
     }
 
     public void launch(String[] args) {
-        if (args.length == 0) {
-            System.out.println("No argument found");
-        } else {
-            String fileNameIn = args[0];
-            String fileNameOut = args[1];
-            File fileIn = new File(fileNameIn);
-            File fileOut = new File(fileNameOut);
-            BufferedReader in = null;
-            PrintWriter out = null;
-            try {
-                in = new BufferedReader(new FileReader(fileIn));
-                out = new PrintWriter(fileOut);
-                String line;
-                while ((line = in.readLine()) != null) {
-                    out.write(line+"\n");
-                }
-            } catch (FileNotFoundException ex) {
-                System.out.println("Sorry, file not found");
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            } finally {
+        File directory = new File(args[args.length-1]);
+        if (!directory.isDirectory())
+            System.out.println("Sorry, the last name does not correspond to a directory");
+        else {
+            int i=0;
+            while (i<args.length-1) {
+                String fileNameIn = args[i];
+                String fileNameOut = directory.getAbsolutePath()+File.separator+args[i];
+                System.out.println(fileNameOut);
+                File fileIn = new File(fileNameIn);
+                File fileOut = new File(fileNameOut);
+                BufferedReader in = null;
+                PrintWriter out = null;
                 try {
-                    if (in != null)
-                        in.close();
-                    if (out != null)
-                        out.close();
+                    in = new BufferedReader(new FileReader(fileIn));
+                    out = new PrintWriter(fileOut);
+                    String line;
+                    while ((line = in.readLine()) != null) {
+                        out.write(line + "\n");
+                    }
+                } catch (FileNotFoundException ex) {
+                    System.out.println("Sorry, file not found");
+                    ex.printStackTrace();
                 } catch (IOException ex) {
                     ex.printStackTrace();
+                } finally {
+                    try {
+                        if (in != null)
+                            in.close();
+                        if (out != null)
+                            out.close();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
                 }
+                i++;
             }
         }
     }
